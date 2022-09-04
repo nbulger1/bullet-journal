3fb75add86e53c3293cc73670203225eb8a2b5c9
Natalie Bulger <bulgern20@gmail.com>
2022-09-04 13:03:53 -0700

    Updated navbar on journal page

--- client/src/components/Journal/index.js
+++ client/src/components/Journal/index.js
@@ -1,14 +1,52 @@
 import React from "react";
 import HTMLFlipBook from "react-pageflip";
 
+const styles = {
+  bookStyle: {
+    border: "black solid 1px",
+  },
+};
+
 function MyJournal(props) {
   return (
-    <HTMLFlipBook width={300} height={500}>
-      <div className="demoPage">Page 1</div>
-      <div className="demoPage">Page 2</div>
-      <div className="demoPage">Page 3</div>
-      <div className="demoPage">Page 4</div>
-    </HTMLFlipBook>
+    <div>
+      <div className="pos-f-t">
+        <div className="collapse" id="navbarToggleExternalContent">
+          <div className="bg-dark p-4">
+            <h4 className="text-white">Collapsed content</h4>
+            <span className="text-muted">Toggleable via the navbar brand.</span>
+          </div>
+        </div>
+        <nav className="navbar navbar-dark bg-dark">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
+      <HTMLFlipBook
+        width={300}
+        height={500}
+        size="fixed"
+        minWidth={315}
+        maxWidth={1000}
+        minHeight={400}
+        maxHeight={1533}
+        maxShadowOpacity={0.5}
+      >
+        <div className="demoPage">Page 1</div>
+        <div className="demoPage">Page 2</div>
+        <div className="demoPage">Page 3</div>
+        <div className="demoPage">Page 4</div>
+      </HTMLFlipBook>
+    </div>
   );
 }
 
